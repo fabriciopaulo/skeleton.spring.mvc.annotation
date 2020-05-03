8475a6f6424f1eac51e0842790ab666b1dd45bb3
Fabricio Paulo <fabricio@fabriciopaulo.com>
2020-05-03 01:30:45 -0300

    SCAVILLE Corp, INC

---
+++ src/main/java/skeleton/spring/mvc/annotation/config/AppConfig.java
@@ -0,0 +1,19 @@
+package skeleton.spring.mvc.annotation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackages = "skeleton.spring.mvc.annotation.controllers")
+public class AppConfig {
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+}
---
+++ src/main/java/skeleton/spring/mvc/annotation/config/SpringServlet.java
@@ -0,0 +1,22 @@
+package skeleton.spring.mvc.annotation.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
